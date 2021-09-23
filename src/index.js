4c50c95db489818b549cdef98cef4d73c934a66f
Maria Maged <maria.maged@pavocom.com>
2021-09-22 22:27:01 -0400

    Commit rever https code because it is not needed

--- src/index.js
+++ src/index.js
@@ -2,14 +2,9 @@ import './server/globals';
 import config from './configs';
 import createApp from './server';
 import database from './database/models';
-import https from 'https';
-import fs from 'fs';
 const app = createApp();
 
-https.createServer({
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')
-}, app).listen(config.port, async () => {
+app.listen(config.port, async () => {
   await database.initializeDatabase();
   console.info('App start', `Started on port ${config.port}`);
 });
